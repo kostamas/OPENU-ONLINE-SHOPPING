7ebfcf478e34da0c370abeb659312123e363662a
kosta mas <kosta.masliansky@gmail.com>
2016-02-20 11:27:05 +0200

    almsot finish with the login/register pags

---
+++ src/java/controller/DataBaseControoler.java
@@ -0,0 +1,12 @@
+
+package controller;
+
+
+public class DataBaseControoler {
+    public static boolean isExits(String item){
+        if(item.equals("pass")){
+            return true;
+        }
+        return false;
+    }
+}
