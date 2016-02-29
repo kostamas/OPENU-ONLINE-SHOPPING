d40a587436ac8742119d9047f7eb306b0ed5293d
kosta mas <kosta.masliansky@gmail.com>
2016-02-29 16:57:42 +0200

    view your store

--- src/java/controller/BuildStoreBean.java
+++ src/java/controller/BuildStoreBean.java
@@ -10,83 +10,19 @@ import static java.awt.PageAttributes.MediaType.C;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URLDecoder;
 import java.nio.file.Files;
 import javax.faces.bean.*;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import model.StoreBuilder;
 
 @ManagedBean
 @ViewScoped
-@WebServlet("/image/*")
-public class BuildStoreBean extends HttpServlet{
+public class BuildStoreBean {
 
     public static int storeId;
     private String storeName;
     private String storePhoto;
     private Part file;
-    String imagePath;
-    
-    public void init() throws ServletException {
-
-        // Define base path somehow. You can define it as init-param of the servlet.
-        this.imagePath = "C:\\onlineShopping";
-
-        // In a Windows environment with the Applicationserver running on the
-        // c: volume, the above path is exactly the same as "c:\var\webapp\images".
-        // In Linux/Mac/UNIX, it is just straightforward "/var/webapp/images".
-    }
-    
-      protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        // Get requested image by path info.
-        String requestedImage = request.getPathInfo();
-
-        // Check if file name is actually supplied to the request URI.
-        if (requestedImage == null) {
-            // Do your thing if the image is not supplied to the request URI.
-            // Throw an exception, or send 404, or show default/warning image, or just ignore it.
-            response.sendError(HttpServletResponse.SC_NOT_FOUND); // 404.
-            return;
-        }
-
-        // Decode the file name (might contain spaces and on) and prepare file object.
-        File image = new File(imagePath, URLDecoder.decode(requestedImage, "UTF-8"));
-
-        // Check if file actually exists in filesystem.
-        if (!image.exists()) {
-            // Do your thing if the file appears to be non-existing.
-            // Throw an exception, or send 404, or show default/warning image, or just ignore it.
-            response.sendError(HttpServletResponse.SC_NOT_FOUND); // 404.
-            return;
-        }
-
-        // Get content type by filename.
-        String contentType = getServletContext().getMimeType(image.getName());
-
-        // Check if file is actually an image (avoid download of other files by hackers!).
-        // For all content types, see: http://www.w3schools.com/media/media_mimeref.asp
-        if (contentType == null || !contentType.startsWith("image")) {
-            // Do your thing if the file appears not being a real image.
-            // Throw an exception, or send 404, or show default/warning image, or just ignore it.
-            response.sendError(HttpServletResponse.SC_NOT_FOUND); // 404.
-            return;
-        }
-
-        // Init servlet response.
-        response.reset();
-        response.setContentType(contentType);
-        response.setHeader("Content-Length", String.valueOf(image.length()));
-
-        // Write image content to response.
-        Files.copy(image.toPath(), response.getOutputStream());
-    }
-
 
     public int getStoreId() {
         return storeId;
