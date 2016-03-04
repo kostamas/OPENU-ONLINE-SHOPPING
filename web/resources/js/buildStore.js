2c9f092684e0e05d5ae075dc08e542823bb9460f
kosta mas <kosta.masliansky@gmail.com>
2016-03-04 14:27:31 +0200

    edit profile

--- web/resources/js/buildStore.js
+++ web/resources/js/buildStore.js
@@ -3,9 +3,9 @@ var toggleProfileProduct = true;
 function toggleEditProfileStore() {
     var editProfile = document.querySelector(".profile-wrapper");
     if (toggleProfileStore) {
-        editProfile.style.right = "75px";
+        editProfile.style.right = "0";
     } else {
-        editProfile.style.right = "-215px";
+        editProfile.style.right = "-270px";
     }
     toggleProfileStore = !toggleProfileStore;
 }
@@ -13,9 +13,9 @@ function toggleEditProfileStore() {
 function toggleEditProfileProduct() {
     var editProfile = document.querySelector(".profile-wrapper");
     if (toggleProfileProduct) {
-        editProfile.style.right = "75px";
+        editProfile.style.right = "0px";
     } else {
-        editProfile.style.right = "-215px";
+        editProfile.style.right = "-270px";
     }
     toggleProfileProduct = !toggleProfileProduct;
 }
