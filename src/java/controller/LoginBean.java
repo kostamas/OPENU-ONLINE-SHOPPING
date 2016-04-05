1a52d9b1bae39f60b82dceaaa7cc163307903227
kosta mas <kosta.masliansky@gmail.com>
2016-04-05 21:07:11 +0300

    login error message

--- src/java/controller/LoginBean.java
+++ src/java/controller/LoginBean.java
@@ -2,16 +2,20 @@ package controller;
 
 import Entities.Administrators;
 import Entities.Users;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.*;
+import javax.faces.context.FacesContext;
 import model.Auth;
 
 @ManagedBean
 @ViewScoped
 public class LoginBean {
 
-    public static String userName ;
-    public static String adminName ;
-    
+    public static String userName;
+    public static String adminName;
+
     private String name;
     private String password;
 
@@ -32,35 +36,44 @@ public class LoginBean {
     }
 
     public String adminLogin() {                 // can't make generice since this fn used in xhtml and can't take paremters
+        FacesContext facesContext = FacesContext.getCurrentInstance();
         String queryName = "Administrators.login";
         String firstQueryParameterName = "adminName";
         String firstParameterValue = this.name;
         String secondQueryParameterName = "password";
         String secondParameterValue = this.password;
-        
+
         this.adminName = this.name;
-        
+
         Auth auth = new Auth();
-        if (auth.loginControl(queryName,firstQueryParameterName,firstParameterValue,secondQueryParameterName,secondParameterValue)) {
+        if (auth.loginControl(queryName, firstQueryParameterName, firstParameterValue, secondQueryParameterName, secondParameterValue)) {
             return "store builder page";
         } else {
+
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("* wrong password or user name"));
             return "";
         }
     }
 
+   
+
     public String userLogin() {                 // can't make generice since this fn used in xhtml and can't take paremters
-    String queryName = "Users.login";
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        String queryName = "Users.login";
         String firstQueryParameterName = "userName";
         String firstParameterValue = this.name;
         String secondQueryParameterName = "password";
         String secondParameterValue = this.password;
-        
         this.userName = this.name;
-        
+
         Auth auth = new Auth();
-        if (auth.loginControl(queryName,firstQueryParameterName,firstParameterValue,secondQueryParameterName,secondParameterValue)) {
+        if (auth.loginControl(queryName, firstQueryParameterName, firstParameterValue, secondQueryParameterName, secondParameterValue)) {
             return "home page";
         } else {
+           
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("* wrong password or user name"));
             return "";
         }
     }
